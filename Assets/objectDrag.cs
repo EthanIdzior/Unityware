c4bd9a9c5506ad1e37b4ea4b61d571068691e8c4
jaranderMTU <70549221+jaranderMTU@users.noreply.github.com>
2020-10-11 18:26:27 -0400

    added dragging, a test sound, and can add property from in game

---
+++ Assets/objectDrag.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectDrag : MonoBehaviour
+{
+    private bool isDragging;
+    bool menuOpen = false;
+    public bool isDraggable = false;
+    public bool mouseOver;
+    Vector2 mousePos;
+
+    private void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+    
+    private void OnGUI()
+    {
+        
+        if (menuOpen)
+       {
+            mousePos = Input.mousePosition; 
+            GUILayout.BeginArea(new Rect(-10, 4, 300, 200), GUI.skin.box);
+            GUILayout.TextField("The Current Value of Draggable is " + isDraggable);
+            if (GUILayout.Button("Set Draggable"))
+            {
+                isDraggable = !isDraggable;
+            }
+
+            if (GUILayout.Button("Close"))
+            {
+                menuOpen = false;
+            }
+
+
+            GUILayout.EndArea();
+
+
+        }
+        
+    }
+    
+
+    public void OnMouseDown()
+    {
+        if (mouseOver)
+        {
+            isDragging = true;
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (mouseOver && Input.GetMouseButton(1))
+        {
+            menuOpen = true;
+        }
+            if (isDragging && isDraggable )
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePos);
+        }
+    }
+}
