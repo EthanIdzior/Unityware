3dd46824462821ef635139d0554f5060bfb70b46
jaranderMTU <70549221+jaranderMTU@users.noreply.github.com>
2020-11-01 13:22:42 -0500

    Added basic title and the ability to return to it

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour
+{
+    public string levelEditor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playLevel()
+    {
+
+    }
+
+    public void createLevel()
+    {
+        SceneManager.LoadScene(levelEditor);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+
+    }
+}
---
+++ Assets/returnToMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class returnToMenu : MonoBehaviour
+{
+    // Variables related to the menu itself
+    int height = 150;
+    int width = 200;
+
+    //needed to return to menu
+    public string menu;
+
+    private bool menuOpen = true;
+
+    // Need to get playing mode
+    public GUIscript playGUI;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Add audio
+ 
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        playGUI = mainCamera.GetComponent<GUIscript>();
+    }
+
+    private void OnGUI()
+    {
+        if (menuOpen && !playGUI.controller.playingGame)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width - width-width, Screen.height - height, width, height), GUI.skin.box);
+
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label("Menu Controls");
+            if (GUILayout.Button("_"))
+            {
+                menuOpen = false;
+            }
+            GUILayout.EndHorizontal();
+
+            // Change Sprites
+            GUILayout.BeginHorizontal("box");
+            if (GUILayout.Button("Return To Menu"))
+            {
+                SceneManager.LoadScene(menu);
+            }
+            if (GUILayout.Button("Save Level"))
+            {
+                
+            }
+            GUILayout.EndHorizontal();
+
+
+
+
+            GUILayout.EndArea();
+        }
+        else if (!playGUI.controller.playingGame)
+        {
+            GUILayout.BeginArea(new Rect(Screen.width - 40-width, Screen.height - 30, 40, 30), GUI.skin.box);
+            if (GUILayout.Button("_"))
+            {
+                menuOpen = true;
+            }
+            GUILayout.EndArea();
+        }
+
+    }
+}
