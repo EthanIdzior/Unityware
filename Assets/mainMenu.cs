40b33af922fa1b4f0612067e5896c9f9e5fb1eb1
Corbin Holz <32336715+crholz@users.noreply.github.com>
2020-11-20 12:08:13 -0500

    Play Mode GUI

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -19,7 +19,27 @@ public class mainMenu : MonoBehaviour
 
     public void playLevel()
     {
+        GameObject canvas = GameObject.Find("Canvas");
 
+        // Create objects for each button
+        GameObject playButton = canvas.gameObject.transform.GetChild(1).gameObject;
+        GameObject play3Button = canvas.gameObject.transform.GetChild(2).gameObject;
+        GameObject quitButton = canvas.gameObject.transform.GetChild(3).gameObject;
+
+        // Change Button Text
+        playButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Select Level";
+        play3Button.GetComponentInChildren<UnityEngine.UI.Text>().text = "Play 3 Random Levels";
+        quitButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Go Back";
+
+        // Remove Listeners
+        playButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.RemoveListener(playLevel);
+        play3Button.GetComponentInChildren<UnityEngine.UI.Button>().onClick.RemoveListener(createLevel);
+        quitButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.RemoveListener(quitGame);
+
+        // Add new functions
+        playButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(selectLevel);
+        play3Button.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(playThree);
+        quitButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(goBack);
     }
 
     public void createLevel()
@@ -31,5 +51,40 @@ public class mainMenu : MonoBehaviour
     {
         Application.Quit();
 
+    }
+
+    public void selectLevel()
+    {
+
+    }
+
+    public void playThree()
+    {
+
+    }
+    
+    public void goBack()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+
+        // Create objects for each button
+        GameObject playButton = canvas.gameObject.transform.GetChild(1).gameObject;
+        GameObject play3Button = canvas.gameObject.transform.GetChild(2).gameObject;
+        GameObject quitButton = canvas.gameObject.transform.GetChild(3).gameObject;
+
+        // Change Button Text
+        playButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Play Levels";
+        play3Button.GetComponentInChildren<UnityEngine.UI.Text>().text = "Create Levels";
+        quitButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Quit Game";
+
+        // Remove Listeners
+        playButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.RemoveListener(selectLevel);
+        play3Button.GetComponentInChildren<UnityEngine.UI.Button>().onClick.RemoveListener(playThree);
+        quitButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.RemoveListener(goBack);
+
+        // Add Listeners
+        playButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(playLevel);
+        play3Button.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(createLevel);
+        quitButton.GetComponentInChildren<UnityEngine.UI.Button>().onClick.AddListener(quitGame);
     }
 }
